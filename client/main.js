0017c1e1b78661facba81b00d55f40bbd5df3337
Rakhesh Krishna P <119130841+rakheshkrishna2005@users.noreply.github.com>
2024-07-23 18:13:30 +0530

    Update main.js

--- client/main.js
+++ client/main.js
@@ -26,8 +26,7 @@ function getBathValue() {
 	var location = document.getElementById("uiLocations");
 	var estPrice = document.getElementById("uiEstimatedPrice");
   
-	var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-	//var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+	var url = "http://127.0.0.1:5000/predict_home_price";
   
 	$.post(url, {
 		total_sqft: parseFloat(sqft.value),
@@ -36,15 +35,14 @@ function getBathValue() {
 		location: location.value
 	},function(data, status) {
 		console.log(data.estimated_price);
-		estPrice.innerHTML = "<h2>₹ " + data.estimated_price.toString() + " Lakhs</h2>";
+		estPrice.innerHTML = "<h2>INR " + data.estimated_price.toString() + " Lakhs</h2>";
 		console.log(status);
 	});
   }
   
   function onPageLoad() {
 	console.log( "document loaded" );
-	var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-	//var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+	var url = "http://127.0.0.1:5000/get_location_names";
 	$.get(url, function(data, status) {
 		console.log("got response for get_location_names request");
 		if(data) {
